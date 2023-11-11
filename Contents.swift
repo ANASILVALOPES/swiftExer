00548d4734ddfb01ae5b662a4e0b4f60d62ee38c
Ana Silva <analuciasantoslopessilva@gmail.com>
2023-11-11 21:37:32 +0000

    commit

---
+++ Contents.swift
@@ -0,0 +1,16 @@
+
+
+let constante = "Steve"
+var  variavel: String?// variavel opcional sem valor definido
+
+variavel = "Jobs"
+
+if let valorDesembrulhado = variavel {
+    print("A constante é \(constante) e a variavel é \(variavel)")
+    
+}
+else{
+    print(" a constante é \(constante) e a variavel é nil")
+}
+
+
